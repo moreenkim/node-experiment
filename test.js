8b4de3701a54936cab9a32899eecc1e5e901497a
moreenkim <moreenkim36@gmail.com>
2020-07-21 09:07:05 +0300

    Add node experiment test code

---
+++ test.js
@@ -0,0 +1,34 @@
+let express = require('express');
+let ourApp = express();
+
+ourApp.use(express.urlencoded({ extended: false }));
+
+ourApp.get('/', function (req, res) {
+  res.send(`
+  <form action= "/answer" method= "POST">
+   <p>What color is the sky on a clear and sunny day?</p>
+   <input name="skyColor"  autocomplete="off">
+   <button>Submit Answer</button>
+  </form>
+  `);
+});
+
+ourApp.post('/answer', function (req, res) {
+  if (req.body.skyColor.toUpperCase() === 'BLUE') {
+    res.send(`
+    <p>congrats! you are correct</p>
+    <a href="/"> Back to homepage</a>
+    `);
+  } else {
+    res.send(`
+    <p>incorrect, try again</p>
+    <a href="/"> Back to homepage</a>
+    `);
+  }
+});
+
+ourApp.get('/answer', function (req, res) {
+  res.send('there is nothng to see');
+});
+
+ourApp.listen(3001);
